c4f2611b725b540e8fdd7d7af5b9f0a7fed207f9
Kurbangalieva_Nailya <nailya22225@gmail.com>
2017-09-11 16:07:07 +0300

    [add] добавила два упражнения

---
+++ 2017/FALL 2017/SEM/Seminar1/Ex1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lilili
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+}
---
+++ 2017/FALL 2017/SEM/Seminar1/Ex2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lilili2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = a / 100 + ((a / 10) % 10) * 10 + (a % 10) * 100;
+
+            Console.WriteLine(b);
+
+
+
+
+        }
+    }
+}
