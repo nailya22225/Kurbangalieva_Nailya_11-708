e8742318c1dfaea373954d7a88d0a5ad8200975f
Kurbangalieva_Nailya <nailya22225@gmail.com>
2017-09-15 14:27:35 +0300

    [add] добавила еще 4 упражнения

---
+++ 2017/FALL 2017/SEM/Seminar1/Ex 3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lilili3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = b - a;
+            c = c / 4 - c / 100 + c / 400;
+            Console.WriteLine(c);
+
+        }
+    }
+}
