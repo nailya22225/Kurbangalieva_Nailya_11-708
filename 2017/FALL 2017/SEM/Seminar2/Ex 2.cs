5accc2836d4ae932a5f1fe770658069420e9f40d
Kurbangalieva_Nailya <nailya22225@gmail.com>
2017-09-20 21:48:34 +0300

    [add] Добавила 2 задания, которые делали в классе и задание, которое надо будет объяснять на уроке:)

---
+++ 2017/FALL 2017/SEM/Seminar2/Ex 2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seminar22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько часов?");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Сколько минут?");
+            double b = Convert.ToInt32(Console.ReadLine());
+            if (a > 12) { a = a - 12;
+            }
+            double d = 5 * a - b;
+     
+            if (d<0)
+            {
+               d = d*-1;
+            }
+            double c = b / 12;
+            double grad = (d - c) * 6;
+            if (grad > 180)
+            {
+                Console.WriteLine(306 - grad);
+            }
+            else { Console.WriteLine(grad);
+            }
+        }
+    }
+}
