60a2ad438638bfb132280a3edacb3847f34225ee
Kurbangalieva_Nailya <nailya22225@gmail.com>
2017-10-05 19:13:05 +0300

    [add] Исправила первое задание из семестровки

--- 2017/FALL 2017/PS/PS1/Ex1.cs
+++ 2017/FALL 2017/PS/PS1/Ex1.cs
@@ -4,23 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace semestr16
+namespace semestr1_
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Задача: По заданному n (от 0 до 100) найти сумму всех n-значных чисел");
-            Console.WriteLine("Введите число n");
-            double n = Convert.ToInt32(Console.ReadLine());
-            double sum = 0;
-            //Math.Pow(10,(n-1))-минимальное n-значное число, например, если число 3-значное, то минимальное=Math.Pow(10,(3-1))=10^2=100
-            //Math.Pow(10,n)-1 -максимальное n-значное число, например, если число 3-значное, то максимальное=Math.Pow(10,3)-1=1000-1=999
-            for (double min = Math.Pow(10, (n - 1)); min < Math.Pow(10, n); min++)
-            {
-                sum += min;
-            }
-            Console.WriteLine(sum);
+            Console.WriteLine("Введите номер билета");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = (a % 10) + ((a / 10) % 10) * 10 + ((a / 100) % 10) * 100;//находим сумму трех последних цифр
+            a = a / 1000;
+            а = (a % 10) + ((a / 10) % 10) * 10 + a / 100*100;//находим сумму первых трех цифр
+            if (Math.Abs(b - a == 1))
+                Console.WriteLine("Является");
+            else 
+                Console.WriteLine("Не является");
         }
     }
 }
