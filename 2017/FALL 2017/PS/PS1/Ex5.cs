77c8695d89e7f77d26900d0550315586e3c69cd1
Rustem Salimov <rust1k@mail.ru>
2017-12-04 14:41:56 +0300

    [check] PS1

--- 2017/FALL 2017/PS/PS1/Ex5.cs
+++ 2017/FALL 2017/PS/PS1/Ex5.cs
@@ -15,10 +15,12 @@ namespace semestr15
             Console.WriteLine("Задача: Найти сумму всех простых делителей заданного натурального числа (<10000)");
             Console.WriteLine("Введите число");
             int a = Convert.ToInt32(Console.ReadLine());
+	    // ---check--- тут как минимум можно было a/2 взять верхней гранью
             for(int i = 1; i <= a; i++)
             {
                 if (a % i == 0) { //ищем делитель числа а
-                    for (int j = 2; j < i; j++) {
+		    // ---check--- опять же до половины можно проходить
+                    for (int j = 2; j < i; j++) { 
                         if (i % j == 0)//проверяем, является ли этот делитель простым
                         {
                             prost = false;
@@ -28,6 +30,7 @@ namespace semestr15
                 if (prost) sum += i;
                     Console.WriteLine(i);
                 }
+		// ---check--- обратно менять значение переменной prost вы не хотите?
             }
             Console.WriteLine("Cумма всех простых делителей равна: "+ sum);
         }
