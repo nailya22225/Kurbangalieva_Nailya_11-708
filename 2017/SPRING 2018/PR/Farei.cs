b037a59f6b643268fe3718ee42b0306e6a539933
Kurbangalieva_Nailya <nailya22225@gmail.com>
2018-02-13 14:55:53 +0300

    [add] добавила ряд Форея

---
+++ 2017/SPRING 2018/PR/Farei.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            RowFarei(n);
+        }
+        public static void RowFarei(int n)
+        {
+            Console.WriteLine("0 / 1");
+            AddFarei(n, 0, 1, 1, 1);
+            Console.WriteLine("1 / 1");
+        }
+
+        private static void AddFarei(int n, int a, int b, int c, int d)
+        {
+            int x = a + c, y = b + d;
+            if (y <= n)
+            {
+                AddFarei(n, a, b, x, y);
+                Console.WriteLine(x+" / "+y);
+                AddFarei(n, x, y, c, d);
+            }
+        }
+    }
+}
