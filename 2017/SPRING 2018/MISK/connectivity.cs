c94331752246b3c84fcea6d787cb90bcaa412cf7
Kurbangalieva_Nailya <nailya22225@gmail.com>
2018-02-27 15:07:17 +0300

    [add] добавила задачу про связность

---
+++ 2017/SPRING 2018/MISK/connectivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите колличество вершин");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                array[i] = i;
+            }
+            while (true)
+            {
+                Console.WriteLine("Введите два числа");
+                int x = Convert.ToInt32(Console.ReadLine());
+                int y = Convert.ToInt32(Console.ReadLine());
+                if (array[x] == array[y])
+                {
+                    Console.WriteLine("Такой путь уже есть");
+                    continue;
+                }
+                for (int j = 0; j < N; j++)
+                {
+                    if (array[j] == array[x])
+                        array[y] = array[x];
+                }
+                Console.WriteLine(x + " " + "-" + " " + y);
+            }
+        }
+    }
+}
