fd40c0003e7518ccbda0d924b48b88417f3be732
Kurbangalieva_Nailya <nailya22225@gmail.com>
2018-03-27 14:58:07 +0300

    [add] Add Linq1

---
+++ 2017/SPRING 2018/PR/Linq1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите k");
+            int k = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Введите колличество чисел в последовательности");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[n];
+
+            Console.WriteLine("Введите последовательность");
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Результат:");
+
+            IEnumerable<int> secondFragment = numbers.Take(k);
+            IEnumerable<int> firstFragment = secondFragment.Where(x => x % 2 == 0);
+            int[] result = firstFragment.ToArray();
+            for (var i = result.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+    }
+}
