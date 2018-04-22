d64ece5081d6a94875089deeb5a37d0f85fd8d6f
nailya22225 <31768249+nailya22225@users.noreply.github.com>
2018-04-23 00:56:26 +0300

    Add files via upload

---
+++ Merge Sort(Linked List).cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSortList
+{
+    class Program
+    {
+        //Делит на части
+        static void Partition(Node head, ref Node front, ref Node back)
+        {
+            Node fast;
+            Node slow;
+
+            if (head == null || head.Next == null)
+            {
+                front = head; 
+                back = null;
+            }
+            else
+            {
+                slow = head;
+                fast = head.Next;
+                while (fast != null)
+                {
+                    fast = fast.Next;
+                    if (fast != null)
+                    {
+                        slow = slow.Next;
+                        fast = fast.Next;
+                    }
+                }
+                front = head; 
+                back = slow.Next;
+                slow.Next = null;
+            }
+        }
+
+        //"Сливает" подмасиивы - переставляет ссылки элементов
+        static Node MergeLists(Node a, Node b)
+        {
+            Node mergedList;
+            if (a == null)
+                return b;
+            else if (b == null)
+                return a;
+
+            if (a.Value <= b.Value)
+            {
+                mergedList = a;
+                mergedList.Next = MergeLists(a.Next, b);
+            }
+            else
+            {
+                mergedList = b;
+                mergedList.Next = MergeLists(a, b.Next);
+            }
+            return mergedList;
+        }
+        //Основное тело сортировки
+        public static void MergeSort(ref Node source)
+        {
+            var head = source;
+            Node a = null;
+            Node b = null;
+            //Если элементов нет или есть только 1, возвращаемся
+            if (head == null || head.Next == null)
+                return;
+
+            Partition(head, ref a, ref b);
+
+            MergeSort(ref a);
+            MergeSort(ref b);
+
+            source = MergeLists(a, b);
+        }
+
+        public static void Main()
+        {
+            MyLinkedList list = new MyLinkedList();
+            var array = File.ReadAllLines("RandomData.txt").Select(x => int.Parse(x)).ToArray();
+            foreach (var e in array)
+                list.Add(e);
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Console.WriteLine(list);
+            MergeSort(ref list.Root);
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch);
+            Console.WriteLine(list);
+        }
+    }
+
+    class Node
+    {
+        //Значение элемента
+        public int Value { get; set; }
+        //Ссылка на следующий
+        public Node Next { get; set; }
+    }
+
+    class MyLinkedList
+    {
+        //Начало
+        public Node Root;
+        //Конец
+        private Node Tail;
+
+        //Добавить новый элемент
+        public void Add(int value)
+        {
+            var node = new Node { Value = value };
+            if (Root == null)
+                Root = Tail = node;
+            else
+            {
+                Tail.Next = node;
+                Tail = node;
+            }
+        }
+        //В строку
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            var current = Root;
+            while (current != null)
+            {
+                result.Append($"{current.Value} ");
+                current = current.Next;
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ Tree Sort.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tree_sort
+{
+    class Node
+    {
+        //key - корень дерева
+        int Key;
+        //правая ветвь
+        Node Right;
+        //левая ветвь
+        Node Left;
+
+        //Создаем дерево
+        public static Node NewNode(int item)
+        {
+            Node temp = new Node();
+            temp.Key = item;
+            temp.Left = temp.Right = null;
+            return temp;
+        }
+
+        //Дрбавляем отсортированные элементы в массив
+        public static void StoreSorted(Node root, int[] arr, ref int i)
+        {
+            if (root != null)
+            {
+                StoreSorted(root.Left, arr,ref i);
+                arr[i++] = root.Key;
+                StoreSorted(root.Right, arr,ref i);
+            }
+        }
+        //Добавляем элемент в дерево
+        public static Node Add(Node node, int key)
+        {
+            //Если дерево пустое, возвращается новый узел
+            if (node == null) return NewNode(key);
+
+            //Если элемент меньше добавляем его в левое дерево, если больше, то в правое
+            if (key < node.Key)
+                node.Left = Add(node.Left, key);
+            else if (key > node.Key)
+                node.Right = Add(node.Right, key);
+
+            //Вернуть указатель (без изменений) узла
+            return node;
+        }
+        //Создаем дерево по исходному массиву
+        public static void TreeSort(int[] arr, int n)
+        {
+            Node root = null;
+
+            //Добавляем все элементы массива в дерево
+            root = Add(root, arr[0]);
+
+            for (int j = 1; j < n; j++)
+                Add(root, arr[j]);
+
+            int i = 0;
+            //Сортируем массив
+            StoreSorted(root, arr, ref i);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = File.ReadAllLines("RandomData.txt").Select(x => int.Parse(x)).ToArray();
+            int n = array.Length;
+            var stop = new Stopwatch();
+            stop.Start();
+            Node.TreeSort(array, n);
+            stop.Stop();
+            Console.WriteLine(stop.Elapsed.TotalMilliseconds);
+            for (int i = 0; i < n; i++)
+                Console.WriteLine( array[i]);
+        }
+    }
+}
