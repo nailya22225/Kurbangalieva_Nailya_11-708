acff7a862a995436d79574083cdbb37a76d15d78
nailya22225 <31768249+nailya22225@users.noreply.github.com>
2018-04-23 00:57:30 +0300

    Add files via upload

---
+++ PatienceSort/ProgramWithMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patiense_sort_11111111
+{
+    class ProgramWithMain
+    {
+    }
+}
